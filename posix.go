20c752dcab4f36f0edb9e8c988373b7cd6b17f82
Marc Bärtschi <19628374+m-baertschi@users.noreply.github.com>
2024-03-25 22:20:59 +0000

    fix linux  build

--- posix.go
+++ posix.go
@@ -1,8 +1,21 @@
-//+build !windows
+//go:build !windows
+// +build !windows
 
 package acl
 
-import "os"
+import (
+	"os"
+
+	"github.com/hectane/go-acl/api"
+)
 
 // Chmod is os.Chmod.
 var Chmod = os.Chmod
+
+func Apply(name string, replace, inherit bool, entries ...api.ExplicitAccess) error {
+	panic("Not Implemented under non Windows Systems")
+}
+
+func GrantName(accessPermissions uint32, name string) api.ExplicitAccess {
+	panic("Not implemented")
+}
